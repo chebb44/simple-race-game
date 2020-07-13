080493466e9d09c9febc25b1404dd9ce81ed8314
Andrey Baryshev <chebb@yandex.ru>
2020-07-13 14:18:28 +0400

    refactor DrawCar.js

--- src/js/drawCar.js
+++ src/js/drawCar.js
@@ -4,78 +4,40 @@ import { UP_DIR, DOWN_DIR } from './constants';
 export function drawCar({
   isDraw, direction, x, y,
 }) {
+  const carUpPixels = [
+    { x: x + 1, y },
+    { x, y: y + 1 },
+    { x: x + 1, y: y + 1 },
+    { x: x + 2, y: y + 1 },
+    { x: x + 1, y: y + 2 },
+    { x, y: y + 3 },
+    { x: x + 2, y: y + 3 },
+  ];
+  const carDownPixels = [
+    { x, y },
+    { x: x + 2, y },
+    { x: x + 1, y: y + 1 },
+    { x, y: y + 2 },
+    { x: x + 1, y: y + 2 },
+    { x: x + 2, y: y + 2 },
+    { x: x + 1, y: y + 3 },
+  ];
   if (direction === UP_DIR) {
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 1,
-      y,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x,
-      y: y + 1,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 1,
-      y: y + 1,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 2,
-      y: y + 1,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 1,
-      y: y + 2,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x,
-      y: y + 3,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 2,
-      y: y + 3,
+    carUpPixels.forEach((item) => {
+      drawPixel.call(this, {
+        isActive: isDraw,
+        x: item.x,
+        y: item.y,
+      });
     });
   }
   if (direction === DOWN_DIR) {
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x,
-      y,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 2,
-      y,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 1,
-      y: y + 1,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x,
-      y: y + 2,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 1,
-      y: y + 2,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 2,
-      y: y + 2,
-    });
-    drawPixel.call(this, {
-      isActive: isDraw,
-      x: x + 1,
-      y: y + 3,
+    carDownPixels.forEach((item) => {
+      drawPixel.call(this, {
+        isActive: isDraw,
+        x: item.x,
+        y: item.y,
+      });
     });
   }
 }
